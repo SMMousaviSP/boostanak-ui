d970494583ad73f162daa5b83237c7b79293611b
SMMousaviSP <smmousavisp@gmail.com>
2019-10-30 19:09:29 +0330

    Install fontawesome and import it to index.js

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+
 
 ReactDOM.render((
 
