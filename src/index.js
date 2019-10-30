a348fa1d315b39a0762e0ddd6d0a7ad585d0f824
SMMousaviSP <smmousavisp@gmail.com>
2019-10-30 19:01:33 +0330

    Install bootstrap and import it to index.js

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 ReactDOM.render((
 
   <BrowserRouter>
