63ee4a8c9d044b347ddeee45cd5bf86810339eac
SMMousaviSP <smmousavisp@gmail.com>
2019-10-30 20:42:08 +0330

    Pass scheme from scheme.json to Navbar component

--- src/components/Main.js
+++ src/components/Main.js
@@ -5,11 +5,15 @@ import { Navbar } from './Components';
 class Main extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {scheme: require("../json/scheme.json")}
   }
 
   render() {
+    let scheme = this.state.scheme;
     return(
-      <Navbar/>
+      <div>
+        <Navbar scheme={scheme} />
+      </div>
     );
   }
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
 
 class Navbar extends React.Component {
   constructor(props) {
@@ -6,29 +8,26 @@ class Navbar extends React.Component {
   }
 
   render() {
+    let scheme = this.props.scheme;
+    let list = [];
+    scheme.files.forEach(function(file, index) {
+      list.push(
+        <li key={index} class="nav-item active">
+          <Link class="nav-link" to={file.name}>{file.name}</Link>
+        </li>
+      );
+    });
+
     return(
       <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
-        <a class="navbar-brand" href="#">Navbar</a>
-
+        <Link class="navbar-brand" to="">Boostanak</Link>
         <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
           <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
-            <li class="nav-item active">
-              <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Link</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-            </li>
+            {list}
           </ul>
-          <form class="form-inline my-2 my-lg-0">
-            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"></input>
-            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-          </form>
         </div>
       </nav>
     );
